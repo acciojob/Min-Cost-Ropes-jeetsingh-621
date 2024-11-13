a434bab6474de791580baa18de2fec9edb331915
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-13 16:31:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 function mincost(arr)
 { 
 //write your code here
+	arr.sort((a,b)=> a-b);
+
+	  let totalCost = 0;
+	while (arr.length>1) {
+		  const first = arr.shift();
+        const second = arr.shift();
+
+		  const cost = first + second;
+		 totalCost += cost;
+
+		 arr.push(cost);
+		  arr.sort((a, b) => a - b);
+		 
+	}
 // return the min cost
-  
+  return totalCost;
 }
 
 module.exports=mincost;
